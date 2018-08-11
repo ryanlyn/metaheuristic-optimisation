ded1c28650d96a0f35ebda0b1d024aa266049bba
ryanlyn <32974205+ryanlyn@users.noreply.github.com>
2018-08-11 13:35:10 +1000

    add initial pso class

---
+++ pso.py
@@ -0,0 +1,29 @@
+import os
+import sys
+
+import numpy as np 
+
+class ParticleSwarm:
+    _version = '1.0'
+    ω = None
+    φ_p = None
+    φ_g = None
+
+
+    def __init__(self, fitness_func, X, X_range, n_particles=100, n_iterations=1000):
+        pass
+
+    def __repr__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def _initialise(self):
+        pass
+
+    def _update_particle_vector(self):
+        pass
+
+    def optimise(self):
+        pass
